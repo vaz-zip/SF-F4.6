d826a111e3ae6decb331cd0d96857c1adf1f3693
KONSNANTIN SHIRYAEV <97960873+vaz-zip@users.noreply.github.com>
2022-11-17 18:23:07 +0400

    Update settings.py

--- Django_Recipes_of_dishes/Recipes_of_dishes/settings.py
+++ Django_Recipes_of_dishes/Recipes_of_dishes/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7)#@)s%abl9yfgk3bn4+z0i5$$t96p38t&6%z&epjiw0#^t!k$'
+SECRET_KEY = '------'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
